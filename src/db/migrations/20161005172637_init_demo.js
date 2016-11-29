81ddf0b131493f08d8a96c1b0e4041d93462c4bd
Yoshi <thangnv77@gmail.com>
2016-11-29 17:21:29 +0700

    Add reference from Content to Resource table

--- src/db/migrations/20161005172637_init_demo.js
+++ src/db/migrations/20161005172637_init_demo.js
@@ -1,16 +1,4 @@
 export async function up(knex) {
-  await knex.schema.createTable('Content', (table) => {
-    table.increments()
-    table.uuid('resourceId').notNullable()
-    table.string('name', 32)
-    table.string('title', 255)
-    table.text('description')
-    table.json('data')
-    table.integer('status').notNullable()
-    table.bigInteger('createdAt')
-    table.bigInteger('updatedAt')
-  })
-
   await knex.schema.createTable('Resource', (table) => {
     table.uuid('id').notNullable().primary()
     table.string('name', 32).notNullable()
@@ -20,9 +8,21 @@ export async function up(knex) {
     table.bigInteger('createdAt')
     table.bigInteger('updatedAt')
   })
+
+  await knex.schema.createTable('Content', (table) => {
+    table.increments()
+    table.uuid('resourceId').notNullable().references('Resource.id')
+    table.string('name', 32)
+    table.string('title', 255)
+    table.text('description')
+    table.json('data')
+    table.integer('status').notNullable()
+    table.bigInteger('createdAt')
+    table.bigInteger('updatedAt')
+  })
 }
 
 export async function down(knex) {
-  await knex.schema.dropTableIfExists('Resource')
   await knex.schema.dropTableIfExists('Content')
+  await knex.schema.dropTableIfExists('Resource')
 }
