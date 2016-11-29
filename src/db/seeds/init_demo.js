c91063e85a66056ecdc2b226e3ac5752f654a22f
Yoshi <thangnv77@gmail.com>
2016-11-29 17:30:17 +0700

    Work around for resource id creation using uuid & add title to content data

--- src/db/seeds/init_demo.js
+++ src/db/seeds/init_demo.js
@@ -2,17 +2,19 @@ import uuid from 'uuid'
 
 export async function seed(knex) {
   await knex('Resource').truncate()
-  const [resourceId1] = await knex('Resource').insert({
-    id: uuid.v4(),
+  const resourceId1 = uuid.v4()
+  await knex('Resource').insert({
+    id: resourceId1,
     name: 'Demo Resource #1',
     mime: 'application/octet-stream',
     md5: '14994fe4b40716b7029045a559ef8922',
     status: 0,
     createdAt: Date.now(),
     updatedAt: Date.now(),
-  }, 'id')
-  const [resourceId2] = await knex('Resource').insert({
-    id: uuid.v4(),
+  })
+  const resourceId2 = uuid.v4()
+  await knex('Resource').insert({
+    id: resourceId2,
     name: 'Demo Resource #2',
     mime: 'application/octet-stream',
     md5: 'f58016895b18052d00e0ae82359a570b',
@@ -20,8 +22,9 @@ export async function seed(knex) {
     createdAt: Date.now(),
     updatedAt: Date.now(),
   }, 'id')
-  const [resourceId3] = await knex('Resource').insert({
-    id: uuid.v4(),
+  const resourceId3 = uuid.v4()
+  await knex('Resource').insert({
+    id: resourceId3,
     name: 'Demo Resource #3',
     mime: 'application/octet-stream',
     md5: 'cc9f2c34a503775c8803ca42bd725e26',
@@ -34,6 +37,7 @@ export async function seed(knex) {
   await knex('Content').insert({
     resourceId: resourceId1,
     name: 'Demo Content #1',
+    title: 'Donec nec placerat purus',
     description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean egestas id quam eget rutrum.',
     data: JSON.stringify({
       foo: 'bar',
@@ -45,6 +49,7 @@ export async function seed(knex) {
   await knex('Content').insert({
     resourceId: resourceId1,
     name: 'Demo Content #2',
+    title: 'Donec nec placerat purus',
     description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean egestas id quam eget rutrum.',
     data: JSON.stringify({
       foo: 'bar',
@@ -56,6 +61,7 @@ export async function seed(knex) {
   await knex('Content').insert({
     resourceId: resourceId2,
     name: 'Demo Content #3',
+    title: 'Donec nec placerat purus',
     description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean egestas id quam eget rutrum.',
     data: JSON.stringify({
       foo: 'bar',
@@ -67,6 +73,7 @@ export async function seed(knex) {
   await knex('Content').insert({
     resourceId: resourceId2,
     name: 'Demo Content #4',
+    title: 'Donec nec placerat purus',
     description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean egestas id quam eget rutrum.',
     data: JSON.stringify({
       foo: 'bar',
@@ -78,6 +85,7 @@ export async function seed(knex) {
   await knex('Content').insert({
     resourceId: resourceId3,
     name: 'Demo Content #5',
+    title: 'Donec nec placerat purus',
     description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean egestas id quam eget rutrum.',
     data: JSON.stringify({
       foo: 'bar',
